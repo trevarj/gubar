3a1e0798c7df99f926577bf404e9cb35580f088d
Trevor Arjeski <tmarjeski@gmail.com>
2025-10-19 00:18:06 +0300

    feat(battery): find or configure power supply device paths
    Use scandir and regex to try to find power_supply devices and also allow for
    overriding with keyword parameters on the battery gublock.
    
    Closes #4

--- gubar/blocks/battery.scm
+++ gubar/blocks/battery.scm
@@ -2,13 +2,27 @@
   #:use-module (gubar gublock)
   #:use-module (gubar swaybar-protocol)
   #:use-module ((ice-9 format) #:prefix fmt:)
+  #:use-module (ice-9 ftw)
+  #:use-module (ice-9 regex)
   #:use-module (ice-9 textual-ports)
   #:use-module (srfi srfi-1)
   #:export (battery))
 
-(define* (battery #:key (format "~a ~a%") (nerd-icons #f))
-  (let ((bat "/sys/class/power_supply/BAT0/capacity")
-        (ac "/sys/class/power_supply/AC/online")
+(define power-supply-path "/sys/class/power_supply/")
+
+(define* (find-power-supply prefix #:optional default-dev)
+  (let ((bat (scandir power-supply-path
+                      (lambda (f) (string-match prefix f)))))
+    (format #f "~a~a" power-supply-path
+            (or (and (pair? bat) (car bat))
+                (format #f "~a~a" prefix default-dev)))))
+
+(define* (battery #:key (format "~a ~a%") (nerd-icons #f)
+                  (bat #f) (ac #f))
+  (let ((bat
+         (string-append (or bat (find-power-supply "BAT" 0)) "/capacity"))
+        (ac
+         (string-append (or ac (find-power-supply "AC")) "/online"))
         (icons '(󰁺 󰁻 󰁼 󰁽 󰁾 󰁿 󰂀 󰂁 󰂂 󰁹)))
     (gublock
      #:block '(("name" . "battery") ("full_text" . "N/A"))
